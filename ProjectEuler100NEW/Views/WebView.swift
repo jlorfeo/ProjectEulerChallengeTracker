9e2229c1fe75bb85cd46d627e77cfc85ece1f2f6
Jan-Erik LORFEO <u074619@canda.com>
2020-03-31 21:46:58 +0200

    Implement Webview component

---
+++ ProjectEuler100NEW/Views/WebView.swift
@@ -0,0 +1,28 @@
+//
+//  WebView.swift
+//  ProjectEuler100NEW
+//
+//  Created by Jan-Erik LORFEO on 22.03.20.
+//  Copyright © 2020 jel. All rights reserved.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let request: URLRequest
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.load(request)
+    }
+}
+
+struct WebView_Previews: PreviewProvider {
+    static var previews: some View {
+        WebView(request: URLRequest(url: URL(string: "https://www.apple.com")!))
+    }
+}
