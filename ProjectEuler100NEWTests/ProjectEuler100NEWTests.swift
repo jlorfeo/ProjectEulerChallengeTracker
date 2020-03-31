97a7d0a74b035d90a4e204df8123c28f7c3ecab9
Jan-Erik LORFEO <u074619@canda.com>
2020-03-31 21:32:32 +0200

    Project setup

---
+++ ProjectEuler100NEWTests/ProjectEuler100NEWTests.swift
@@ -0,0 +1,34 @@
+//
+//  ProjectEuler100NEWTests.swift
+//  ProjectEuler100NEWTests
+//
+//  Created by Jan-Erik LORFEO on 22.03.20.
+//  Copyright © 2020 jel. All rights reserved.
+//
+
+import XCTest
+@testable import ProjectEuler100NEW
+
+class ProjectEuler100NEWTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
